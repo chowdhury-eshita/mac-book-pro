8cafea0498231a2b639780915678ce9f6df00384
chowdhury-eshita <chowdhuryeshita58@gmail.com>
2021-08-21 07:00:48 +0600

    initial

---
+++ js/app.js
@@ -0,0 +1,12 @@
+//extra memory cost
+document.getElementById('memory-16').addEventListener('click', function () {
+    const extraMemory = document.getElementById('extra-memory-cost').innerText = 180;
+});
+//extra storage
+document.getElementById('storage-512').addEventListener('click', function () {
+    const extraMemory = document.getElementById('extra-storage-cost').innerText = 100;
+});
+//extra storage 1tb
+document.getElementById('storage-1tb').addEventListener('click', function () {
+    const extraMemory = document.getElementById('extra-storage-cost').innerText = 180;
+});
